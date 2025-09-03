ada83800f55cf2201928571e9482eba425672b5f
ttldtor <ttldtor@gmail.com>
2025-09-03 22:34:09 +0300

    Add bits v0.1.0

--- packages/b/bits/xmake.lua
+++ packages/b/bits/xmake.lua
@@ -4,7 +4,10 @@
     set_description("Library for bit manipulation")
     set_license("BSL-1.0")
 
-    add_urls("https://github.com/ttldtor/bits.git")
+    add_urls("https://github.com/ttldtor/bits/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/ttldtor/bits.git")
+
+    add_versions("v0.1.0", "d2fdf2f63f62a1e9f77a0c228fe4ad0234a6d4d8a4e4f24ffb6c478d2c7a876f")
 
     on_install(function (package)
         os.cp("include/bits", package:installdir("include"))
