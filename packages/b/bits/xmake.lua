318c9ba112fd8b562c480eb2b4ea3d0e5d1030ed
ttldtor <ttldtor@gmail.com>
2025-09-03 21:32:39 +0300

    First

---
+++ packages/b/bits/xmake.lua
@@ -0,0 +1,12 @@
+﻿package("bits")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/ttldtor/bits")
+    set_description("Library for bit manipulation")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/ttldtor/bits.git", {branch = "default"})
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+package_end()
