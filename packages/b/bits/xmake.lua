192bebb3c32554d223e87b9a17846c0cbeed67cf
ttldtor <ttldtor@gmail.com>
2025-09-30 21:54:02 +0300

    Add bits v1.0.0

--- packages/b/bits/xmake.lua
+++ packages/b/bits/xmake.lua
@@ -13,6 +13,7 @@ package("bits")
     add_versions("v0.1.0", "d2fdf2f63f62a1e9f77a0c228fe4ad0234a6d4d8a4e4f24ffb6c478d2c7a876f")
     add_versions("v0.2.0", "463bdfc86486185948f5abf800b5d74992b3c032465da068dd11f6485d69a726")
     add_versions("v0.3.0", "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed")
+    add_versions("v1.0.0", "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed")
 
     on_install(function (package)
         os.cp("include/bits", package:installdir("include"))
